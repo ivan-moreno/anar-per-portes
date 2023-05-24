ccb34bfce0eba2d2e41b6647c4ea832a5ddd1e04
Iván Moreno <ivanmoreno1312@gmail.com>
2023-05-24 18:34:05 +0200

    Inventory UI now turns semi-transparent when unused for a while

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -6,6 +6,9 @@ namespace AnarPerPortes
     {
         [SerializeField] private Transform slotsGroup;
         [SerializeField] private GameObject slotPrefab;
+        private CanvasGroup slotsCanvasGroup;
+        private float timeSinceItemChange;
+        private const float timeToUnfocus = 8f;
 
         public void GenerateSlotFor(InventoryItem item)
         {
@@ -19,7 +22,31 @@ namespace AnarPerPortes
                 return;
             }
 
+            item.OnEquipped.AddListener(ResetTimeSinceItemChange);
+            item.OnUnequipped.AddListener(ResetTimeSinceItemChange);
+
             slot.Initialize(item);
+        }
+
+        private void Start()
+        {
+            slotsCanvasGroup = slotsGroup.GetComponent<CanvasGroup>();
+        }
+
+        void ResetTimeSinceItemChange()
+        {
+            timeSinceItemChange = 0f;
+        }
+
+        private void Update()
+        {
+            timeSinceItemChange += Time.deltaTime;
+
+            var shouldFocus = timeSinceItemChange < timeToUnfocus;
+            slotsCanvasGroup.alpha = Mathf.MoveTowards(
+                current: slotsCanvasGroup.alpha,
+                target: shouldFocus ? 1f : 0.3f,
+                maxDelta: shouldFocus ? Time.unscaledDeltaTime * 4f : Time.unscaledDeltaTime * 0.5f);
         }
     }
 }
